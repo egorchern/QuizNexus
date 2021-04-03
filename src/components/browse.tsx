fc71e575e415c37603343fb9c33e61fcced63276
Egor Chernyshev <egorch.formal@gmail.com>
2021-04-03 19:01:15 +0100

    some work on browse interface

--- src/components/browse.tsx
+++ src/components/browse.tsx
@@ -8,7 +8,41 @@ export class Browse extends React.Component{
     render(){
         return (
             <div className="browse">
-
+                <div className="browse_toolbar">
+                    <div className="browse_input_container">
+                        <span>
+                            Name:
+                        </span>
+                        <input className="form-control">
+                        </input>
+                    </div>
+                    <div className="browse_input_container">
+                        <span>
+                            Time to complete (minutes):
+                        </span>
+                        <div className="two_column_grid">
+                            <input className="form-control" placeholder="min:">
+                            </input>
+                            <input className="form-control" placeholder="max:">
+                            </input>
+                        </div>
+                        
+                    </div>
+                    <div className="browse_input_container">
+                        <span>
+                            Difficulty:
+                        </span>
+                        <select className="form-select">
+                            <option>Easy</option>
+                            <option>Medium</option>
+                            <option>Hard</option>
+                        </select>
+                    </div>
+                    
+                </div>
+                <div className="browse_results">
+                    <span>Results</span>
+                </div>
             </div>
         )
     }
