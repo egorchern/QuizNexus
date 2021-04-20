eff93995838cf764ee8a7a56533883c83487ab95
Egor Chernyshev <egorch.formal@gmail.com>
2021-04-20 21:39:24 +0100

    recompile dist

--- dist/app.5793fd45.js
+++ dist/app.5793fd45.js
@@ -42419,7 +42419,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64941" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65219" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
